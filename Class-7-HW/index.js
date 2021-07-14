fabd1aa22f5d1129aeab1ee80bffe8047979439e
Bryan88888 <yingyu.yan@hotmail.com>
2021-07-14 16:43:03 -0400

    commit

--- Class-7-HW/index.js
+++ Class-7-HW/index.js
@@ -1,59 +1,48 @@
 const isArray = (array) => {
-    return console.log (Array.isArray(array))
-}
+  return console.log(Array.isArray(array));
+};
 
 const joinArray = (thingThatGetsJoined) => {
-    thingThatGetsJoined.split(',').join($)
-    return thingThatGetsJoined
+  thingThatGetsJoined.split(",").join($);
+  return thingThatGetsJoined;
 };
 
 const SubjectNeededToBeFound = [1, 2, 3, 4, 5];
 
 const indexFinder = (SubjectNeededToBeFound, numberOfThingNeededToBeFound) => {
-    return console.log(SubjectNeededToBeFound[numberOfThingNeededToBeFound])
-}
-
-
-
+  return console.log(SubjectNeededToBeFound[numberOfThingNeededToBeFound]);
+};
 
 const add = (addingArr) => {
-    const addingArr = []
-//???
+  const addingArr = [];
+  //???
 };
-
-
 
 const mult = (multiplyingArr) => {
-    const multiplyingArr = []
-///???
+  const multiplyingArr = [];
+  ///???
 };
-
 
 const removeDuplicate = (arrInput) => {
-    const checkingArr = [];
-    for (let i = 0; i < arrInput.length; i++)
-        checkingArr.unshift(i); {
-        for (let v = 0; i < checkingArr;) {
-            if ((arrInput[i] === checkingArr[v]))
-                arrInput.split[i];
-                
-        }
+  const checkingArr = [];
+  for (let i = 0; i < arrInput.length; i++) checkingArr.unshift(i);
+  {
+    for (let v = 0; i < checkingArr; ) {
+      if (arrInput[i] === checkingArr[v]) arrInput.split[i];
     }
-    return console.log(arrInput);
+  }
+  return console.log(arrInput);
 };
-
 
 const leapYearCalculator = (year) => {
-    let leapYearNumber = 4
-    const years = [2000, 2018]
-    let output = []
-    for (let i = years[1]; i < years[2]; i++) {
-        output.push(years[1] + i * 4)
-    }
-        
-    return console.log(output);
-        
-};
+  let leapYearNumber = 4;
+  const years = [2000, 2018];
+  let output = [];
+  for (let i = years[1]; i < years[2]; i++) {
+    output.push(years[1] + i * 4);
+  }
 
+  return console.log(output);
+};
 
 //idk last one
